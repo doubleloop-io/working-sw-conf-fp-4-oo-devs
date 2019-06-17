2f16cf20e364035b1169e0228df6008d53172fb2
Andrea Vallotti <andrea.vallotti@gmail.com>
2019-06-17 19:05:48 +0200

    Add option skeleton classes

---
+++ csharp/Fp4OoDevelopers/Functional/Option.cs
@@ -0,0 +1,23 @@
+﻿namespace Fp4OoDevelopers.Functional
+{
+    public abstract class Option<T>
+    {
+    }
+
+    public class Some<T> : Option<T> 
+    {
+        private readonly T value;
+
+        public Some(T value)
+        {
+            this.value = value;
+        }
+    }
+
+    public class None<T> : Option<T>
+    {
+        public None()
+        {
+        }
+    }
+}
