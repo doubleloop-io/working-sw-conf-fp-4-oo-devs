db764fbd2b280f359689a8df1fcb46dcc4e6e9f9
Andrea Vallotti <andrea.vallotti@gmail.com>
2019-06-24 14:39:52 +0200

    Tests form Either equality

---
+++ csharp/Fp4OoDevelopers.Tests/Functional/EitherTests.cs
@@ -0,0 +1,19 @@
+﻿using Fp4OoDevelopers.Functional;
+using Xunit;
+
+namespace Fp4OoDevelopers.Tests.Functional
+{
+    public class EitherTests
+    {
+        [Fact]
+        public void Equality()
+        {
+            Either<string, int> right = new Right<string, int>(42);
+            Either<string, int> left = new Left<string, int>("test");
+
+            Assert.NotEqual(right, left);
+            Assert.Equal(right, new Right<string, int>(42));
+            Assert.Equal(left, new Left<string, int>("test"));
+        }
+    }
+}
