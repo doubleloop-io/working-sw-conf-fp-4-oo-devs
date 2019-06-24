f8fbe69a8b14dad3ff28321bd678d0e18a0a7187
Andrea Vallotti <andrea.vallotti@gmail.com>
2019-06-24 09:27:09 +0200

    Add either skeleton classes

---
+++ csharp/Fp4OoDevelopers/Functional/Either.cs
@@ -0,0 +1,26 @@
+﻿namespace Fp4OoDevelopers.Functional
+{
+    public abstract class Either<TLeft, TRight>
+    {
+    }
+
+    public class Left<TLeft, TRight> : Either<TLeft, TRight>
+    {
+        private readonly TLeft value;
+
+        public Left(TLeft value)
+        {
+            this.value = value;
+        }
+    }
+
+    public class Right<TLeft, TRight> : Either<TLeft, TRight>
+    {
+        private readonly TRight value;
+
+        public Right(TRight value)
+        {
+            this.value = value;
+        }
+    }
+}
