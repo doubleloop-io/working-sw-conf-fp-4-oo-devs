dafdb910cc9bf15c360a6f9ec99ce236bf9dbab6
Andrea Vallotti <andrea.vallotti@gmail.com>
2019-06-24 16:00:48 +0200

    Implement Either.FlatMap

--- csharp/Fp4OoDevelopers/Functional/Either.cs
+++ csharp/Fp4OoDevelopers/Functional/Either.cs
@@ -6,6 +6,8 @@ namespace Fp4OoDevelopers.Functional
     {
         public abstract Either<TLeft, TOut> Map<TOut>(Func<TRight, TOut> func) where TOut : class;
 
+        public abstract Either<TLeft, TOut> FlatMap<TOut>(Func<TRight, Either<TLeft, TOut>> func) where TOut : class;
+
         public static implicit operator Either<TLeft, TRight>(TLeft value) => new Left<TLeft, TRight>(value);
 
         public static implicit operator Either<TLeft, TRight>(TRight value) => new Right<TLeft, TRight>(value);
@@ -24,6 +26,8 @@ namespace Fp4OoDevelopers.Functional
         {
             return value;
         }
+
+        public override Either<TLeft, TOut> FlatMap<TOut>(Func<TRight, Either<TLeft, TOut>> func) => value;
 
         public override bool Equals(object obj) =>
             obj is Left<TLeft, TRight> left && Equals(left.value, value);
@@ -45,6 +49,9 @@ namespace Fp4OoDevelopers.Functional
         {
             return func(value);
         }
+
+        public override Either<TLeft, TOut> FlatMap<TOut>(Func<TRight, Either<TLeft, TOut>> func) =>
+            func(value);
 
         public override bool Equals(object obj) =>
             obj is Right<TLeft, TRight> right && Equals(right.value, value);
