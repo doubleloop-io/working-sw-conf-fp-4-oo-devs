d702b31627e1ecffeb2dc876b49daf012d5fc54e
Andrea Vallotti <andrea.vallotti@gmail.com>
2019-06-24 11:06:03 +0200

    Implement Option.Pure

--- csharp/Fp4OoDevelopers/Functional/Option.cs
+++ csharp/Fp4OoDevelopers/Functional/Option.cs
@@ -2,6 +2,8 @@
 {
     public abstract class Option<T>
     {
+        public static Option<T> Pure(T value) =>
+            value == null ? (Option<T>)new None<T>() : new Some<T>(value);
     }
 
     public class Some<T> : Option<T> 
@@ -12,6 +14,12 @@
         {
             this.value = value;
         }
+
+        public override bool Equals(object obj) => 
+            obj is Some<T> some && Equals(value, some.value);
+
+        public override int GetHashCode() => 
+            value.GetHashCode();
     }
 
     public class None<T> : Option<T>
@@ -19,5 +27,10 @@
         public None()
         {
         }
+
+        public override bool Equals(object obj) =>
+            obj is None<T>;
+
+        public override int GetHashCode() => 0;
     }
 }
