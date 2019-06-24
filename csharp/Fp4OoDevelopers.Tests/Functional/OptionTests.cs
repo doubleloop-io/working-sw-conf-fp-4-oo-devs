e04b308ab8a43656db975f679a3594e6b338abbe
Andrea Vallotti <andrea.vallotti@gmail.com>
2019-06-24 10:58:30 +0200

    Tests for Option.Pure

---
+++ csharp/Fp4OoDevelopers.Tests/Functional/OptionTests.cs
@@ -0,0 +1,24 @@
+﻿using Fp4OoDevelopers.Functional;
+using Xunit;
+
+namespace Fp4OoDevelopers.Tests.Functional
+{
+    public class OptionTests
+    {
+        [Fact]
+        public void PureOfSomething()
+        {
+            Option<string> result = Option<string>.Pure("test");
+
+            Assert.Equal(new Some<string>("test"), result);
+        }
+
+        [Fact]
+        public void PureOfNull()
+        {
+            Option<string> result = Option<string>.Pure(null);
+
+            Assert.Equal(new None<string>(), result);
+        }
+    }
+}
