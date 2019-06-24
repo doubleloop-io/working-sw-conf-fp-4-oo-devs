d95938b94ba2313f1b9da59ce3ef9a044ab9dd6d
Andrea Vallotti <andrea.vallotti@gmail.com>
2019-06-24 15:44:43 +0200

    Tests for natural transformation from option to either

---
+++ csharp/Fp4OoDevelopers.Tests/Functional/NaturalTransformationTests.cs
@@ -0,0 +1,28 @@
+﻿using Fp4OoDevelopers.Functional;
+using Xunit;
+
+namespace Fp4OoDevelopers.Tests.Functional
+{
+    public class NaturalTransformationTests
+    {
+        [Fact]
+        public void SomeToEither()
+        {
+            var option = new Some<int>(42);
+
+            Either<string, int> result = option.ToEither("error");
+
+            Assert.Equal(new Right<string, int>(42), result);
+        }
+
+        [Fact]
+        public void NoneToEither()
+        {
+            var option = None<int>.Instance;
+
+            Either<string, int> result = option.ToEither("error");
+
+            Assert.Equal(new Left<string, int>("error"), result);
+        }
+    }
+}
